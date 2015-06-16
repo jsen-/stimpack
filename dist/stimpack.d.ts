5e96729e91695672d5ab628fe2dcc836263ec3c6
jsen- <max.enhanced@gmail.com>
2015-06-16 16:02:34 +0200

    CR/LF

--- dist/stimpack.d.ts
+++ dist/stimpack.d.ts
@@ -1,16 +1,13 @@
-declare module 'stimpack/src/stimpack' {
+declare module "stimpack" {
+
 	export interface Annotable extends Function {
-	    annotations?: Annotable[];
-	    propAnnotations?: Map<string | symbol, Annotable>;
+		annotations?: Annotable[];
+		propAnnotations?: Map<string | symbol, Annotable>;
 	}
 	export class Injector {
-	    private types;
-	    constructor();
-	    get(Type: Annotable): any;
+		private types;
+		constructor();
+		get(Type: Annotable): any;
 	}
 	export function Inject(target: Annotable | Object, propertyKey?: string | symbol): void;
-
-}
-declare module 'stimpack' {
-	export * from 'stimpack/src/stimpack';
 }
